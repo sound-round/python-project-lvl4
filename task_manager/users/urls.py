7bf533df674b86dc336f5290990568eb9b3dc267
denis <yudaev1@gmail.com>
2021-11-18 18:03:34 +0300

    Add registration form

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from task_manager.users import views
 
+
 urlpatterns = [
-    path('', views.UsersList.as_view(), name='users-list')
+    path('', views.UsersList.as_view(), name='users-list'),
+    path('create/', views.UserCreate.as_view(), name='create-user'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 
 
@@ -9,3 +11,14 @@ class UsersList(ListView):
     template_name = "users/list.html"
     context_object_name = 'users_list'
     # paginate_by = 15
+
+
+class UserCreate(CreateView):
+
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'password']
+    template_name = "users/user_form.html"
+
+    def get_success_url(self):
+        return reverse('index')
+
