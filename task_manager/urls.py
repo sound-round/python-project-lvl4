2cef74b5b6c51c663a84941ba41d55b2cc158e13
denis <yudaev1@gmail.com>
2021-11-23 18:18:08 +0300

    Create statuses app

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,7 +20,10 @@ from task_manager import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+
     path('users/', include('task_manager.users.urls')),
+    path('statuses/', include('task_manager.statuses.urls')),
+
     path('login/', views.user_login, name='user-login'),
     path('logout/', views.user_logout, name='user-logout'),
 ]
