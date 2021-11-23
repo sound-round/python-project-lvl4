7b6df960cd07726a06a9b3a6a024bfad6e4b9794
denis <yudaev1@gmail.com>
2021-11-23 18:18:41 +0300

    Add statuses/tests.py

---
+++ task_manager/statuses/tests.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.test import Client
+from task_manager.statuses.models import Status
+
+class StatusesTest(TestCase):
+    
+    client = Client()
+
+    def set_up(self):
+        status = {"name": "new"}
+        return self.client.post('/statuses/create/', status)
+
+    def test_status_create(self):
+        response = self.set_up()
+        status = Status.objects.get(name="new")
+        self.assertTrue(isinstance(status, Status))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual('new', status.name)
+
+    def test_status_update(self):
+        self.set_up()
+        status = Status.objects.get(name="new")
+        status_id = status.id
+        update_data = {'name': 'done'}
+        response = self.client.post(f'/statuses/{status_id}/update/', update_data)
+        status = Status.objects.get(id=status_id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(status.name, 'done')
+
+    def test_status_delete(self):
+        self.set_up()
+        status = Status.objects.get(name="new")
+        status_id = status.id
+        response = self.client.post(f'/statuses/{status_id}/delete/')
+        self.assertEqual(Status.objects.count(), 0)
+        self.assertEqual(response.status_code, 302)
